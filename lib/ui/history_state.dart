afadf7cb5e12a6665bc92baed684fdc85740cdec
djamiirr <djamiirr3@gmail.com>
2019-08-24 16:34:48 +0100

    commented code

--- lib/ui/history_state.dart
+++ lib/ui/history_state.dart
@@ -13,20 +13,23 @@ class HistoryScreen extends StatefulWidget{
 
 class _HistoryState extends State<HistoryScreen>{
 
-  List<File> _files = List();
+  List<File> _files = List();//creating list of text files
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    
+
+    //listing directory content
     _dirContents(Directory(widget.path)).then((list){
       list.forEach((file){
+        //putting the object in files list if it isa file
        if (file is File)
          setState(() {
            _files.add(file);
          });
       });
+      //sorting list by name (created date time)
       _files.sort((file1, file2) {
         return file2.path.compareTo(file1.path);
       });
@@ -40,10 +43,12 @@ class _HistoryState extends State<HistoryScreen>{
       appBar: AppBar(title: Text("History"),),
       body: ListView.builder(itemBuilder: (context, index){
         final file = _files[index];
+        //getting file name
         final title = file.path.substring(file.path.lastIndexOf('/')+1);
         return ListTile(
           title: Text(title,),
           onTap: () async {
+            //getting text from file and send it to textview screen
             final text = await file.readAsStringSync();
             Navigator.push(context, MaterialPageRoute(builder: (context) => TextViewScreen(title: title, text: text,)));
           },
@@ -54,12 +59,11 @@ class _HistoryState extends State<HistoryScreen>{
   }
 
   Future<dynamic> _dirContents(Directory dir) {
-    var files = <FileSystemEntity>[];
-    var completer = new Completer();
-    var lister = dir.list(recursive: false);
+    List<FileSystemEntity> files = <FileSystemEntity>[];
+    final completer = new Completer();
+    final lister = dir.list(recursive: false);
     lister.listen (
             (file) => files.add(file),
-        // should also register onError
         onDone:   () => completer.complete(files)
     );
     return completer.future;
--- lib/ui/main_state.dart
+++ lib/ui/main_state.dart
@@ -7,7 +7,6 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:location/location.dart';
 import 'package:path_provider_ex/path_provider_ex.dart';
 import 'package:permission/permission.dart';
-import 'package:track_me/utils/dimens.dart';
 import 'history_state.dart';
 
 class MainScreen extends StatefulWidget {
@@ -15,8 +14,10 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainState extends State<MainScreen> with TickerProviderStateMixin {
-  double _heigt, _width, _lat, _lng;
-  int _isTracking = 0;
+  double _width,
+      _lat, _lng; //storing last latitude and longitude to prevent listener to create the same position on the file by checking if the new position is different than the old one
+
+  int _isTracking = 0; //0: not tracking, 1: Starting, 2: tracking.
   Location _location;
   File _file;
   IOSink _open;
@@ -29,20 +30,27 @@ class _MainState extends State<MainScreen> with TickerProviderStateMixin {
     // TODO: implement initState
     super.initState();
 
+    //requesting storage permission
     _requestStoragePermission();
 
+    //getting SDCard path
     _getPath();
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    _heigt = size.height;
     _width = size.width;
     // TODO: implement build
     return WillPopScope(
       onWillPop: () async {
+        //when user press back button on Android, the app will show a dialog asking user if he want to fully exit the
+        //application or just minimize it and let the location tracking in the background. to use this feature there is a
+        //method channel in the MainActivity.java which listen for "sendToBackground" invoked method and move task to back
+
+        //initializing exit or minimize status
         bool exit = false;
+
         await showDialog(
             context: context,
             builder: (context) {
@@ -51,14 +59,18 @@ class _MainState extends State<MainScreen> with TickerProviderStateMixin {
                 actions: <Widget>[
                   MaterialButton(
                     onPressed: () {
+                      //invoking "sendToBackground" method to send task to back
                       _moveTaskToBackChannel.invokeMethod("sendToBackground");
+                      //closing dialog
                       Navigator.pop(context);
                     },
                     child: Text("minimize"),
                   ),
                   MaterialButton(
                     onPressed: () {
+                      //setting exit to true to fully close the application
                       exit = true;
+                      //closing dialog
                       Navigator.pop(context);
                     },
                     child: Text("exit"),
@@ -66,7 +78,7 @@ class _MainState extends State<MainScreen> with TickerProviderStateMixin {
                 ],
               );
             });
-        print(exit);
+        //if exit is true the the navigator will close the app, elase, it will do nothing.
         return Future.value(exit);
       },
       child: Scaffold(
@@ -113,7 +125,7 @@ class _MainState extends State<MainScreen> with TickerProviderStateMixin {
                         height: _width * .5,
                         child: Stack(
                           children: <Widget>[
-                            Center(
+                            Center(//animations behind the main yellow button.
                               child: _isTracking == 0
                                   ? Container()
                                   : _isTracking == 1
@@ -134,12 +146,17 @@ class _MainState extends State<MainScreen> with TickerProviderStateMixin {
                             ),
                             Center(
                               child: InkWell(
+
                                 onTap: () {
-                                  if (_isTracking != 1) {
+
+                                  if (_isTracking != 1) {//to prevent user to press the button when locating is sill starting
                                     setState(() {
                                       _isTracking = _isTracking == 2 ? 0 : 1;
                                     });
-                                    _requestLocating();
+                                    if (_isTracking == 0)
+                                      _requestLocating();
+                                    else
+                                      _startStopLocationg();
                                   }
                                 },
                                 child: Container(
@@ -180,8 +197,12 @@ class _MainState extends State<MainScreen> with TickerProviderStateMixin {
                       ),
                       Padding(
                         padding: const EdgeInsets.only(top: 8.0),
-                        child:
-                            Text(_isTracking == 2 ? formatDate(_duration, [HH, ':', nn, ':', ss]) : "", style: TextStyle(color: Colors.white),),
+                        child: Text(
+                          _isTracking == 2
+                              ? formatDate(_duration, [HH, ':', nn, ':', ss])
+                              : "",
+                          style: TextStyle(color: Colors.white),
+                        ),
                       )
                     ],
                   ),
@@ -196,6 +217,7 @@ class _MainState extends State<MainScreen> with TickerProviderStateMixin {
 
   @override
   void dispose() {
+    //disposing the location listener
     _location.dispose().then((_) {
       print(_);
       super.dispose();
@@ -203,34 +225,44 @@ class _MainState extends State<MainScreen> with TickerProviderStateMixin {
   }
 
   void _requestLocating() {
+    //initializing location if it's null
     if (_location == null) _location = Location();
+
+    //checking for location permission
     _location.hasPermission().then((granted) {
       if (!granted)
-        _location.requestPermission().then((status) {
+        _location.requestPermission().then((status) {//permission not granted
+          //requesting permission
           if (!status)
-            _requestLocating();
+            _requestLocating(); //re request permission if denied
           else
             _location.requestService().then((val) {
-              if (val)
-                _startStopLocationg();
-              else
-                _requestLocating();
+              // request to enable location service
+              if (val) //location service enabled
+                _startStopLocationg(); //Start locating...
+              else //location service not enabled
+                _requestLocating(); //re request enable location service
             });
         });
-      else
+      else //permission granted
         _location.requestService().then((val) {
-          if (val)
-            _startStopLocationg();
+          if (val)//location service enabled
+            _startStopLocationg();//Start locating...
           else
-            _requestLocating();
+            _requestLocating(); //location service not enabled => re request to enable location service
         });
     });
   }
 
   Future _startStopLocationg() async {
+    //if is not tracking
     if (_isTracking == 1) {
+
+      //preparing file
+      //getting date
       final date = DateTime.now();
 
+      //prepare file location (path + datetime + .txt)
       _file = File("$_path${formatDate(date, [
         yy,
         '-',
@@ -244,73 +276,106 @@ class _MainState extends State<MainScreen> with TickerProviderStateMixin {
         ":",
         ss
       ])}.txt");
+
+      //creating file in storage
       await _file.create(recursive: true);
+
+      //opening the file
       _open = _file.openWrite();
+
+      //update ui to set (application is locating)
       setState(() {
         _isTracking = 2;
       });
+
+      //resetting the counter
       _duration = DateTime(0);
+
       _startCounter();
+
+      //resetting old latitude and old longitude to null;
       _lat = null;
       _lng = null;
-      _location.onLocationChanged().listen((location) {
-        if (_lat == null) {
-          _file.writeAsStringSync(
-              "${location.latitude},${location.longitude}\n",
-              mode: FileMode.append);
-        }
-        else if(location.latitude != _lat || location.longitude != _lng){
-          _file.writeAsStringSync(
-              "${location.latitude},${location.longitude}\n",
-              mode: FileMode.append);
-        }
-        _lat = location.latitude;
-        _lng = location.longitude;
-      });
-    } else {
+
+      //starting location listener
+      _startLocationListener();
+    } else { //if the app is tracking
       setState(() {
+        //updating the state to set => app is not locating
         _isTracking = 0;
       });
+      //disposing location listener (library modification)
       _location.dispose().then((val) {
-        print(val);
       });
+
+      //closing file.
       _open.flush();
       _open.close();
     }
   }
 
   Future<String> _getPath() async {
+    //requesting for SDCard path
     final storageInfo = await PathProviderEx.getStorageInfo();
+    //saving path
     _path = storageInfo[0].rootDir + "/track me files/";
+    //returning path
     return storageInfo[0].rootDir + "/track me files/";
   }
 
   Future _requestStoragePermission() async {
+    //defining current platform
     if (Platform.isIOS) {
+      //checking permission status
       final isGranted =
           await Permission.getSinglePermissionStatus(PermissionName.Storage);
       if (isGranted != PermissionStatus.allow ||
           isGranted != PermissionStatus.always) {
-        final result =
-            await Permission.requestSinglePermission(PermissionName.Storage);
+        //if denied, request for it
+       await Permission.requestSinglePermission(PermissionName.Storage);
       }
     } else if (Platform.isAndroid) {
+      //checking permission status
       final isGranted =
           await Permission.getPermissionsStatus([PermissionName.Storage]);
       if (isGranted != PermissionStatus.allow ||
           isGranted != PermissionStatus.always) {
-        final result =
-            await Permission.requestPermissions([PermissionName.Storage]);
+        //if denied, request for it
+        await Permission.requestPermissions([PermissionName.Storage]);
       }
     }
   }
 
   Future _startCounter() async {
-    while (_isTracking == 2) {
-      await Future.delayed(Duration(seconds: 1));
+    while (_isTracking == 2) { //while the app is tracking
+      await Future.delayed(Duration(seconds: 1));//waiting for one second
       setState(() {
-        _duration = _duration.add(Duration(seconds: 1));
+        _duration = _duration.add(Duration(seconds: 1));//adding one seccond to the counter duration
       });
     }
+  }
+
+  void _startLocationListener() {
+    //on location changed
+    _location.onLocationChanged().listen((location) {
+
+      //if old position is recently initialized to null then create the new position the file.
+      if (_lat == null) {
+        //writing position to the file
+        _file.writeAsStringSync(
+            "${location.latitude},${location.longitude}\n",
+            mode: FileMode.append);
+      } else if (location.latitude != _lat || location.longitude != _lng) {
+        //if the new position is dfferent than the old, create the position to the file.
+        _file.writeAsStringSync(
+            "${location.latitude},${location.longitude}\n",
+            mode: FileMode.append);
+      }
+
+      //storing last position to compare it to the comming one.
+      _lat = location.latitude;
+      _lng = location.longitude;
+
+    });
   }
 }
